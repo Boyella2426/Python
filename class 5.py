b46ee38c34dace1f55007cf2d673d8d7069323db
Boyella2426 <donu.b2004@gmail.com>
2026-03-24 20:42:29 +0530

    class 5

---
+++ class 5.py
@@ -0,0 +1,102 @@
+
+""" ======================================================== nested if =============================================================================="""
+
+num1,num2,num3= map(int,input().split())
+if (num1<num2<num3):
+   print("num1 is smaller")
+elif (num1>num2<num3):
+   print("num2 is smaller")
+elif(num1>num2>num3):
+   print("num3 is smaller")
+else:
+   print("all are equal")
+   
+   
+
+
+"""============================================================LOOPS============================================================================================
+# LOOP : it a is repeatative task,it exceutiveswith different values Until unless it reaches to certain conditions.
+
+ # TYPES OF LOOP:
+  #1:FOR LOOP
+  #2:WHILE LOOP
+
+"""
+
+"""
+num1,num2,num3=map(int,input("enter numbers: ").split())
+if num1 % 2==0:
+    print(" num1 is even")
+elif num2 %2!=0:
+    print("num2 is odd")
+elif num3 >=0 and num3 %2==0:
+     print("num3 is odd")
+else:
+    print("all are even or all or odd")
+"""
+
+
+"""
+lst=[11,12,13,14,15]
+for ind in range(0,5,1):
+    if lst[ind]% 2 == 0:
+         print(lst[ind],"is even ")
+    else:
+         print(lst[ind],"is odd")
+
+"""
+
+
+"""
+#print even numbers between 20 - 40
+
+
+for num in range(20,41):
+    if num %2==0:
+        print(num)
+
+for num in range(20,41,2):
+    print(num)
+
+"""
+
+
+"""
+#print 1-100 numbers
+
+for num in range(1,101):
+    print(num, end =" " )
+
+"""
+
+
+ 
+"""
+#find sum of n natural numbers
+
+n=int(input())
+total=0
+for num in range (1,n+1):
+    total = total+num
+    print (total)
+
+"""
+
+"""=============================================================================  PRACTICE  ====================================================================="""
+
+#print 1 to 100 numbers
+# print even numbers between 10 to 40
+# print odd numbersbetween  1 to 50
+# find which even or odd from lodt of numbers
+#sum of even numbers upto 'n'
+
+
+#print 1 to 100 numbers:
+
+
+
+
+
+
+
+    
